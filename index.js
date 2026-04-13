72f1ce22a8f8867943f46c17064f7f34dda774b3
roberte <robertonsate23@gmail.com>
2026-04-14 01:17:46 +0300

    flatiron book store dom

--- index.js
+++ index.js
@@ -44,4 +44,23 @@ const bookStore = {
 }
 
 // Write your code here!
+const bookStoreTitle = document.getElementById('header');
+bookStoreTitle.textContent = bookStore.name;
 
+const bookList = document.getElementById('book-list');
+bookList.innerHTML = '';
+bookStore.books.forEach(book => {
+    const bookContainer = document.createElement('li');
+    const bookTitle = document.createElement('h3');
+    const bookAuthor = document.createElement('p');
+    const bookImage = document.createComment('img');
+
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = book.author;
+    bookImage.src = book.imageUrl;
+
+    bookContainer.appendChild(bookTitle);
+    bookContainer.appendChild(bookAuthor);
+    bookContainer.appendChild(bookImage);
+    bookList.appendChild(bookContainer);
+});
